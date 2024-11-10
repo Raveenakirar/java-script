f210e96e987837f6c9ad54dc3c579da5d9833b16
Raveena kirar <raveenakirar2@gmail.com>
2024-11-10 11:47:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+//console.log(...)
+//undefined
+//null
+//NaN  (not a number)
+
+//operator
+//arthmetic(+,-)
+//relation(==,<=,>=,===,!=,!==)
+//logical(and(&&),or(||),not(!))
+// var i="undefined";
+// console.log(i +null);
+
+
+//let age1 = "24"
+//let age2 = "24"
+
+//let res = age1==age2 && !0 && age2
+//console.log(res);
+// OUTPUT = 24
+
+//let l = "JS"
+//let msg = `Hello , I am learning ${l}`
+//console.log(msg);
+// OUTPUT = Hello , I am learning JS
