7c946b669bf61aa44bb235774e0126cf2e96783c
Raveena kirar <raveenakirar2@gmail.com>
2024-11-10 11:44:34 +0530

    Add files via upload

---
+++ Assignment3.js
@@ -0,0 +1,103 @@
+//-1|  Given the array of days of the week:
+//  let days = ["Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday", "Sunday"];
+// // Extract the weekend days from the array
+// let day = days.slice(-1);
+// console.log(day);
+// output = [ 'Sunday' ]
+
+// -2 |  Given the array of temperatures:
+//  let temperatures = [30, 32, 35, 28, 27, 31, 29];
+//  // Create a new array containing the middle three temperatures
+//  let newarr= temperatures.slice(2,5);
+//  console.log(newarr);
+//  output = [ 35, 28, 27 ]
+
+// -3 |  You have an array of favorite sports:
+//  let sports = ["Cricket", "Football", "Hockey"];
+//  // Add "Badminton" to the end of the array and remove "Football".
+//  sports.push("Badminton");
+//  let newsports = sports.slice(1,2);
+//  console.log(sports,newsports);
+
+//-4 | You are managing a list of fruits:
+//  let fruits = ["Apple", "Banana", "Mango"];
+//  //Insert "Orange" at the second position.
+//   name = ["Apple","Orange","Banana","Mango"];
+//   let [s0,s1,...rest]=name;
+//  console.log(name);
+// output = [ 'Apple', 'Orange', 'Banana', 'Mango' ]
+
+//-5 | You have an array of students:
+//  let students = [
+//     { name: "Rahul", age: 20, major: "Computer Science" },
+//    { name: "Priya", age: 22, major: "Mathematics" },
+//     { name: "Amit", age: 21, major: "Physics" }
+//     ];
+//    // Print the names and majors of each student
+//   for(let student of students){
+//     console.log(student.name,"=",student.major);
+//   } output = ahul = Computer Science
+//              Priya = Mathematics
+//              Amit = Physics
+  
+// -6 | Given the following array of products:
+//  let products = [
+//     { name: "Laptop", price: 800, inStock: true },
+//     { name: "Smartphone", price: 500, inStock: false },
+//     { name: "Tablet", price: 300, inStock: true }
+//     ];
+//     // Create a new array containing only the products that are in stock
+//     let newarr = products.filter(product=>product.inStock);
+//     console.log(newarr);
+//     output = [
+//         { name: 'Laptop', price: 800, inStock: true },
+//         { name: 'Tablet', price: 300, inStock: true }
+//       ]
+
+// - 7 |Given an array of customer names:
+//  let customers = ["Aarav", "Vihaan", "Reyansh"];
+//  //Print a welcome message for each customer
+//  for(let customer of customers){
+//     console.log("Welcome = ",customer); 
+//  }  output = Welcome =  Aarav
+//  Welcome =  Vihaan
+//  Welcome =  Reyansh
+
+// -8 | You have an array of city names:
+//  let cities = ["Delhi", "Mumbai", "Bengaluru"];
+//  // Convert each city name to uppercase and print it.
+// //   let word = arr[index];
+//  for(let word of cities){
+//  let words = word.toUpperCase();
+//  console.log(words);
+// } output = DELHI  MUMBAI BENGALURU
+ 
+// - 9 |  9. Given a string of comma-separated items:
+//  let items = "Pen, Pencil, Notebook, Eraser";
+//    // Split the string into an array and count the number of items
+// let newarr = items.split('');
+// console.log(newarr,newarr.length);
+// output = [
+//     'P', 'e', 'n', ',', ' ', 'P',
+//     'e', 'n', 'c', 'i', 'l', ',',
+//     ' ', 'N', 'o', 't', 'e', 'b',
+//     'o', 'o', 'k', ',', ' ', 'E',
+//     'r', 'a', 's', 'e', 'r'
+//   ] 29
+
+// - 10 | You have a filename:
+// let filename = "report.pdf";
+ // Check if the filename starts with "report"
+
+ // -11 | Given the following URLs:
+ let urls = ["<https://google.com>", "<http://example.com>", 
+    "<https://github.com>"];
+ //  Create a new array containing only the URLs that start with "https"
+ 
+ 
+
+ 
+
+  
+
+ 
