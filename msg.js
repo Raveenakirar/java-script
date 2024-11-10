916545aae8e118b4a1491b4e8d5166c6bc3d2c3a
Raveena kirar <raveenakirar2@gmail.com>
2024-11-10 11:49:20 +0530

    Add files via upload

---
+++ msg.js
@@ -0,0 +1,47 @@
+let msg="hello all, i am here"
+//let out = msg.split(" ")
+//console.log(out)
+// OUTPUT = ['hello', 'all,', 'i', 'am', 'here']
+
+//let out = msg.split('')
+//console.log(out)
+  //   OUTPUT =    ['h', 'e', 'l', 'l', 'o',
+  //                ' ', 'a', 'l', 'l', ',',
+  //                ' ', 'i', ' ', 'a', 'm',
+  //                ' ', 'h', 'e', 'r', 'e'  ]
+//let out = msg.split('')  
+//out=out.reverse()
+//msg =out.join(" ")
+//console.log(msg)
+// OUTPUT = e r e h   m a   i   , l l a   o l l e h
+
+//let out = msg.split(" ")  
+//out=out.reverse()
+//msg =out.join(" ")
+//console.log(msg)
+// OUTPUT = here am i all, hello
+
+//let ages=[32,34,334,45,454,5]
+//for(let age of ages){
+//  console.log(age/2);
+//}  
+//OUTPUT =  16
+//          17
+//          167
+//          22.5
+//          227
+//          2.5
+
+//msg="hello all , i am here";
+//let words = msg.split(" ")
+//for(let wIndex in words){
+//let word = words[wIndex]
+//word = word.split("")
+//word = word.reverse()
+//word = word.join("")
+//words[wIndex]=word
+//}
+//console.log(words.join(" "))
+// OUTPUT = olleh lla , i ma ereh
+
+    
