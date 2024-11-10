ee4ba417e7a81aa3101f9174c5d1cf729b433cbd
Raveena kirar <raveenakirar2@gmail.com>
2024-11-10 11:46:56 +0530

    Add files via upload

---
+++ homework2.js
@@ -0,0 +1,60 @@
+ //const arr = ["Apple","Banana","Cherry","Date"];
+
+// Create new array of first three values from this array?
+//    const newarr =arr.slice(0,3);
+//    console.log(newarr)
+// Create new array of alternative values from this array?
+//    const newarr =arr.filter((Value,index )=> index % 2==0);
+//    console.log(newarr);
+   
+// Update the value of this array in lower case?
+// for(let words of arr){
+// let arr1=words.toLowerCase();
+// console.log(arr1);
+// }
+// 4 Convert array String to lowercase if the length of the string is even , or to uppercase if the length is odd:
+    //  for (let index in arr){
+    //       let word = arr[index];
+    //      if (index % 2===0){
+    //          console.log(word.toLowerCase());
+    //       }  else{  
+    //          console.log( word.toUpperCase());
+    //       }
+    //  }
+// question -2
+//    const prices=[120,80,200,300,150,50];
+//    let totalPrice =0;
+//    for(let price of prices){
+//     totalPrice +=price
+//    }
+//    let finalPrice = prices.length>=5? totalPrice * 0.5 : totalPrice;
+//    console.log(finalPrice);
+   
+
+//   // question -3
+//     const products =['apple','banana','orange','apple','banana'];
+// //    let uniqueArr =Array.from(new Set(product));
+// //    console.log(uniqueArr);
+//     let uniqueproducts =[]
+//     for(let item of products){
+//         if(!uniqueproducts.includes(item))uniqueproducts.push(item);
+//     }
+//      console.log(uniqueproducts);
+
+   //questions -4
+    // const str ="World Health Organization"
+    // const acronym =str.split(" ").map(word => word[0]).join("");
+    // console.log(acronym )
+    // let result = " ";
+    // const words = str.split(" ");
+    // for (let w of words){
+    //     result += w[0]
+    // }
+    //  console.log(result)
+    //  // q-5
+    //  let str1 = "listen"
+    //  let str2 = "silent"
+    //  str1 = str1.split("").sort().join("")
+    //  str2 = str2.split("").sort().join("")
+    //  if ()
+
