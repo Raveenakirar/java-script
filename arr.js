8d3b37554368bf1406d6982604005bbf7990f679
Raveena kirar <raveenakirar2@gmail.com>
2024-11-10 11:43:40 +0530

    Add files via upload

---
+++ arr.js
@@ -0,0 +1,51 @@
+//let s1="rubi"
+//let s2="ruhi"
+//let s3="sakshi"
+//let s4="ritik"
+let student=["rubi","ruhi","sakshi","ritik","kiran"]
+//console.log(student.slice(student.length-2));
+//OUTPUT = [ 'ritik', 'kiran' ]
+
+//methods of array
+//1.slice
+
+//let res = student.slice(0,3)
+//console.log(res);
+// OUTPUT = ['rubi','ruhi','sakshi']
+
+//let res1=student.slice(student.length-2 ,student.length);
+//console.log(res1);
+//OUTPUT = [ 'ritik', 'kiran' ]
+
+// let res = student.slice(-6,-2)
+// console.log(res);
+// OUTPUT =[ 'rubi', 'ruhi', 'sakshi' ]
+
+//push,slice(start,end number),splice,indexof(return of the index of element),includes(it is the return of true or false)
+//map(return),forEach(no return),filter,find,some,every,sort,toSrted
+
+// GIVEN THE ARRAY 
+//let  marks=[45,67,89,87,65,43]
+
+//let out =marks.slice(1, 3)
+// OUTPUT = [ 67, 89 ]
+
+//let out = marks.splice(1,3,12,14)  //insert ,delete,replace
+//OUTPUT = [ 67, 89, 87 ] [ 45, 12, 14, 65, 43 ]
+
+//let out=marks.indexOf(89)
+// OUTPUT = 2
+
+//let out=marks.includes(89)
+//OUTPUT = true
+
+//marks.push(34,55,77)
+//output [ 45, 67, 89, 87, 65, 43, 34, 55, 77 ]
+
+//marks.sort()
+  //OUTPUT  [ 43, 45, 65, 67, 87, 89 ]
+
+//  console.log(marks);
+//  console.log(out); 
+//  console.log(out,marks); 
+//  console.log(out); 
