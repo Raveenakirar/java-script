8e14a4f30c90e57fe012328008b3ff630b3303de
Raveena kirar <raveenakirar2@gmail.com>
2024-11-10 11:48:56 +0530

    Add files via upload

---
+++ newque.js
@@ -0,0 +1,58 @@
+// question 1 
+// const grocery_list =["milk","bread","eggs","butter","cheese"]
+// let grocery_list1 = grocery_list.push("fruits");
+// let grocery_list1 = grocery_list.splice(3,0,"vegetable")
+// let grocery_list1 = grocery_list.splice(1,1)
+// let grocery_list1 = grocery_list.indexOf("chesse");
+// let grocery_list1 = grocery_list.sort()
+//  console.log(grocery_list);
+
+ 
+//question 2 
+// const movies =["jawan","Rocky Aur Rani Ki Prem Kahani "," Gadar 2","Pathaan","Zara Hatke Zara Bachke"]
+// let movies1= movies.push(" OMG 2")
+// console.log(movies);
+
+// if(movies.includes("Pathaan")){
+//     console.log("exist");
+// }else{
+//     console.log("not exist")
+// }
+
+// let movies1 = movies.splice(0,4,"The Vaccine War")
+// console.log(movies);
+
+// let movies1 = movies.reverse();
+// console.log(movies.length);
+
+// questions 3 
+// const attendance =["Aarav","Isha","Rohan","Ananya","Kabir"]
+// let attendance1=attendance.push("Neha")
+// let attendance1 = attendance.splice(0,1)
+   
+// console.log(attendance);
+
+// question 4
+
+const expenses =[1200,2500,800,400,300]
+// let expenses1 = expenses.push("500");
+// let expenses1 = expenses.splice(1,0,"100")
+// let expension = expenses.splice(-1)
+// let sum = expenses.reduce((firstValue ,currentValue) => firstValue + currentValue)
+// console.log( +sum);
+let maxValue =  Math.max(...expenses);
+let minValue = Math.min(...expenses);
+console.log("Maximum number is => "+ maxValue);
+console.log ("Minimum number is => "+ minValue);
+
+// question 5
+// const playlist = ["Kesariya","Pasoori","Raatan Lambiyan","Tere Pyaar Main "]
+// let playlist1= playlist.push("Apna Bana Le");
+// let playlist1 = playlist.splice(1,1)
+// let playlist1 = playlist.splice(3,4,"Chaleya")
+// console.log(playlist.length)
+// let playlist1 = playlist.reverse();
+// console.log(playlist);
+
+
+
